c5b5d227b3572fbec341fef692878d2b365222aa
ElKaTuFv <m.lukmanisma@gmail.com>
2024-04-17 23:12:52 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD LARAVEL 10</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gradient-to-r from-black to-blue-700">
+    <h1 class="text-center text-white text-3xl mt-7 ">MASUK KE HALAMAN POST</h1>
+    <a href="/posts"><button class="border text-3xl text-white mx-auto flex p-2 rounded-md bg-green-900 mt-3 ">GASSS</button></a>
+</body>
+</html>
